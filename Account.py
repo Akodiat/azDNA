d9b8dd29f8c4066d482ac1f26ad6cb1348e761a1
Gamer43 <towerdefence1000@gmail.com>
2020-04-13 17:19:05 -0700

    added a deleted unverified API

--- Account.py
+++ Account.py
@@ -14,6 +14,7 @@ find_status_by_user_id_query = ("SELECT status FROM Users WHERE id = %s")
 get_verify_code_query = ("SELECT verifycode FROM Users WHERE id = %s")
 verify_user = ("UPDATE Users SET verified = %s WHERE id = %s")
 get_username_query("SELECT username FROM Users WHERE id = %s")
+delete_unverified = ("DELETE FROM Users WHERE verified = False AND creationDate > %s)
 
 def getEmail(userId):
     cursor.execute(find_email_by_user_id_query(userId))
@@ -39,16 +40,22 @@ def getUsername(userId):
     cursor.execute(get_username_query(userId))
     return cursor.username
 
+#CreationTime in seconds
+def deleteUnverified(CreationTime):
+    cursor.execute(deleted_unverified(int(time.time() - CreationTime)))
+    cnx.commit()
+
 #checks verification code for user
 def verifyUser(userId, VerifyCode):
-    #not sure how mysql.connector works, so wrapping in try catch just in case
+    #not sure how mysql.connector works if entry does not exist/none found, so wrapping in try catch just in case
     try:
         #query the database for the user's verification code
         iterator = cursor.execute(get_verify_code_query(userId))
         #check that they match
         if(iterator.verifycode == VerifyCode):
             #verify the user
-            cursor.execute(verifyuser("True", userId))
+            cursor.execute(verify_user("True", userId))
+            cnx.commit()
             return True;
         else:
             return False;
